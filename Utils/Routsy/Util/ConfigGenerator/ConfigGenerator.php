e3a48c689bc2d4cbd5eb938271942326a1ce0fd7
lingtalfi <lingtalfi@gmail.com>
2017-04-20 07:05:48 +0200

    update

--- Utils/Routsy/Util/ConfigGenerator/ConfigGenerator.php
+++ Utils/Routsy/Util/ConfigGenerator/ConfigGenerator.php
@@ -5,7 +5,9 @@ namespace Kamille\Utils\Routsy\Util\ConfigGenerator;
 
 
 use ArrayExport\ArrayExport;
+use Bat\FileSystemTool;
 use Kamille\Utils\ModuleInstallationRegister\ModuleInstallationRegister;
+use LinearFile\LineSetFinder\BiggestWrapLineSetFinder;
 
 class ConfigGenerator
 {
@@ -30,30 +32,56 @@ class ConfigGenerator
 
 
         $installedModules = ModuleInstallationRegister::getInstalled();
-        a($installedModules);
         $changed = false;
+        $newRoutesDynamic = [];
+        $newRoutesStatic = [];
+
+
         foreach ($installedModules as $mod) {
             $modConfFile = $this->modulesTargetDir . "/$mod/routsy/conf.php";
             if (file_exists($modConfFile)) {
                 $routes = [];
                 include $modConfFile;
 
+                $lines = file($modConfFile);
+
                 foreach ($routes as $id => $route) {
                     // we only override routes that don't exist (don't want to accidentally override the user's work)
                     if (!array_key_exists($id, $_routes)) {
-                        $_routes[$id] = $route;
+
+                        // doesn't exist?
+                        // ok, is it dynamic or static (make two groups)
+                        $routeContent = $this->getRouteContent($id, $lines);
+
+                        if (true === $this->isDynamic($route[0])) {
+                            $newRoutesDynamic[$id] = $route;
+                        } else {
+                            $newRoutesStatic[$id] = $route;
+                        }
                         $changed = true;
                     }
                 }
             }
         }
 
-        if (true === $changed) {
-            a($_routes);
-            $newContent = ArrayExport::export($_routes, 2);
-            a($newContent);
-        }
 
+        // append in static Section
+        // append in dynamic Section
+
+
+//        if (true === $changed) {
+//            $routesArr = ArrayExport::export($_routes, 2);
+//
+//            $newContent = <<<EEE
+//<?php
+//use Kamille\Architecture\Request\Web\HttpRequestInterface;
+//
+//\$routes = $routesArr;
+//EEE;
+//
+//
+//            FileSystemTool::mkfile($confFile, $newContent);
+//        }
     }
 
     public function setConfFile($confFile)
@@ -68,5 +96,26 @@ class ConfigGenerator
         return $this;
     }
 
+    //--------------------------------------------
+    //
+    //--------------------------------------------
+    private function isDynamic($uri)
+    {
+        return (false !== strpos($uri, '{'));
+    }
+
+    private function getRouteContent($routeId, array $lines)
+    {
+        $pat = '!^\$routes\[([^\]]+)\]\s*=!';
+        $lineSets = BiggestWrapLineSetFinder::create()
+            ->setPrepareNameCallback(function ($v) {
+                return substr($v, 1, -1);
+            })
+            ->setNamePattern($pat)
+            ->setStartPattern($pat)
+            ->setPotentialEndPattern('!\];!')
+            ->find($lines);
+        return $lineSets;
+    }
 
 }
