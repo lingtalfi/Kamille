e9ed00b6d789b2fe2b1e29a85fc37754b0a74268
lingtalfi <lingtalfi@gmail.com>
2017-05-12 14:42:07 +0200

    update

--- Utils/Laws/Theme/LawsThemeInterface.php
+++ Utils/Laws/Theme/LawsThemeInterface.php
@@ -4,10 +4,18 @@
 namespace Kamille\Utils\Laws\Theme;
 
 
+use Kamille\Architecture\Response\Web\HttpResponseInterface;
 use Kamille\Utils\Laws\Config\LawsConfig;
 
 interface LawsThemeInterface
 {
 
     public function configureView($viewId, LawsConfig $config);
+
+    /**
+     * @param $pageId
+     * @return HttpResponseInterface
+     */
+    public function renderByPageId($pageId);
+
 }
