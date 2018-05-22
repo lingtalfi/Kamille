7d852194ef31ae2adc79fb1745d931956d74218f
lingtalfi <lingtalfi@gmail.com>
2018-05-22 17:22:41 +0200

    update

--- Architecture/Response/Web/PdfResponse.php
+++ Architecture/Response/Web/PdfResponse.php
@@ -7,10 +7,21 @@ namespace Kamille\Architecture\Response\Web;
 class PdfResponse extends HttpResponse
 {
 
+    protected $fileName;
+
+
     public function __construct($content = "", $code = 200)
     {
         parent::__construct($content, $code);
-        header('Content-Type: application/pdf');
+        header("Content-type:application/pdf");
+
+
+    }
+
+    public function setFileName(string $fileName)
+    {
+        $fileName = str_replace('"', '\"', $fileName);
+        header("Content-Disposition:attachment;filename=$fileName");
     }
 
 
